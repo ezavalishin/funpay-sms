e733c8197bf29bdba20ada48914cb53279b35bfc
ezavalishin <ezavalishin@gmail.com>
2019-11-03 02:36:55 +0400

    parser

---
+++ SmsParser.php
@@ -0,0 +1,81 @@
+<?php
+
+class WalletNotFoundException extends \Exception
+{
+}
+
+class SumNotFoundException extends \Exception
+{
+}
+
+class TokenNotFoundException extends \Exception
+{
+}
+
+class SmsParser
+{
+    private $string;
+    private $wallet;
+    private $sum;
+    private $token;
+
+    public function __construct(string $string)
+    {
+        $this->string = $string;
+
+        $this->findWallet();
+        $this->findSum();
+        $this->findToken();
+    }
+
+    private function clearString(string $remove): void
+    {
+        $this->string = str_replace($remove, "", $this->string);
+    }
+
+    private function findWallet(): void
+    {
+        if (!preg_match('/\d{11,20}/m', $this->string, $matches)) {
+            throw new WalletNotFoundException();
+        }
+
+        $this->wallet = $matches[0];
+        $this->clearString($this->wallet);
+    }
+
+    private function findSum(): void
+    {
+        if (!preg_match('/(\d+[,.]\d{1,2})|(\d+\s?[р])/mu', $this->string, $matches)) {
+            throw new SumNotFoundException();
+        }
+
+        $this->sum = $matches[0];
+        $this->sum = (float)str_replace(',', '.', $this->sum);
+        $this->clearString($matches[0]);
+    }
+
+    private function findToken(): void
+    {
+        if (!preg_match('/\d{4,6}/m', $this->string, $matches)) {
+            throw new TokenNotFoundException();
+        }
+
+        $this->token = $matches[0];
+        $this->clearString($this->token);
+    }
+
+    public function getWallet(): string
+    {
+        return $this->wallet;
+    }
+
+    public function getSum(): float
+    {
+        return $this->sum;
+    }
+
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+}
